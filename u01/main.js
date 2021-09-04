d252bb9be9cbd384a5466ef6471655752aca2b64
jul-star <jstar@rambler.ru>
2021-09-04 12:24:35 +0300

    first commit

---
+++ u01/main.js
@@ -0,0 +1,42 @@
+const {app, BrowserWindow} = require('electron')
+// include Node.js 'path' module
+const path = require('path')
+let mainWindow
+process.env.NODE_ENV = 'development'
+
+const isDev = process.env.NODE_ENV != 'production' ? true : false
+// console.log(process.platform)
+const isWin = process.platform == 'win32' ? true : false
+
+app.commandLine.appendSwitch('trace-warnings')
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+        title: 'ImageShrink',
+        width: 500,
+        height: 600,
+        // webPreferences: {
+        //     preload: path.join(__dirname, 'preload.js')
+        // }
+        resizable : isDev
+    })
+    
+    mainWindow.loadFile('index.html')   
+    // mainWindow.loadURL("https://rbc.ru")
+}
+
+// app.on()
+app.whenReady().then(() => {
+    createMainWindow()
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+      })
+
+      app.on('window-all-closed', function () {
+        if (process.platform !== 'darwin') app.quit()
+      })
+  })
+
+  app.allowRendererProcessReuse = true
+
